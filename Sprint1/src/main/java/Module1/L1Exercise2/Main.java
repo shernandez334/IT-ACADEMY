f6a63d25224bc1186a20e76f791f1f58c3247922
santiago <santiagoso1998@gmail.com>
2024-07-21 14:03:32 +0200

    L1exercise2

---
+++ Sprint1/src/main/java/Module1/L1Exercise2/Main.java
@@ -0,0 +1,9 @@
+package Module1.L1Exercise2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Car.brake());
+        Car car1 = new Car("Testarrosa");
+        System.out.println(car1.accelerate());
+    }
+}
