754c29b5867ca003348e1f950a25bf186865c5e9
pepe <blanco@blanco.com>
2024-07-31 13:32:12 +0200

    L2exercise7

---
+++ Sprint1/src/test/java/L2exercise7/TestObject.java
@@ -0,0 +1,15 @@
+package L2exercise7;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.Optional;
+
+public class TestObject {
+
+    @Test
+    public void TestEmptyObject(){
+        Optional<String> emptyObject = Optional.empty();
+        Assertions.assertThat(emptyObject).isEmpty();
+    }
+}
