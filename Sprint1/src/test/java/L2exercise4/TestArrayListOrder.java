7538759a8f04673a577a22201982b653d7423a55
pepe <blanco@blanco.com>
2024-07-31 12:23:21 +0200

    L2exercise5

--- Sprint1/src/test/java/L2exercise4/TestArrayListOrder.java
+++ Sprint1/src/test/java/L2exercise4/TestArrayListOrder.java
@@ -3,9 +3,11 @@ package L2exercise4;
 import Module4_Testing.L2exercise4.Car;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
-
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TestArrayListOrder {
 
@@ -20,11 +22,17 @@ public class TestArrayListOrder {
                         .thenComparingInt(Car::getPrice));
     }
 
+    @Test
     public void testArrayListInAnyOrder(){
-        ArrayList<Car> Dealership = new ArrayList<Car>();
-        Dealership.add(new Car("Ferrari", 300000));
-        Dealership.add(new Car("Mercedes", 300000));
-        Dealership.add(new Car("Mercedes", 250000));
-        Assertions.assertThat()
+        boolean areEqualLists;
+        ArrayList<Car> dealerShipListOfCars = new ArrayList<>();
+        dealerShipListOfCars.add(new Car("Ferrari", 300000));
+        dealerShipListOfCars.add(new Car("Mercedes", 300000));
+        dealerShipListOfCars.add(new Car("Mercedes", 250000));
+        List<Car> dealerShipSortedListOfCars = dealerShipListOfCars.stream()
+                .sorted(Comparator.comparing(Car::getBrand).thenComparingInt(Car::getPrice))
+                .toList();
+        areEqualLists = dealerShipListOfCars.equals(dealerShipSortedListOfCars);
+        Assertions.assertThat(areEqualLists).isFalse();
     }
 }
---
+++ Sprint1/src/test/java/L2exercise5/testMap.java
@@ -0,0 +1,18 @@
+package L2exercise5;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class testMap {
+    @Test
+    public void testAddingKeyToMap(){
+        boolean keyExist;
+        Map<String, Integer> testMap = new HashMap<>();
+        testMap.put("One", 1);
+        keyExist = testMap.containsKey("One");
+        Assertions.assertThat(keyExist).isTrue();
+    }
+}
