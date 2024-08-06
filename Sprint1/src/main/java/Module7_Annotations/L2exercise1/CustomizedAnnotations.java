d5d6d1d66909874ec3511c9b68a82df3c0cead04
santiago <santiagoso1998@gmail.com>
2024-08-06 22:20:45 +0200

    L2exercise1

--- Sprint1/src/main/java/Module7_Annotations/L2exercise1/CustomizedAnnotations.java
+++ Sprint1/src/main/java/Module7_Annotations/L2exercise1/CustomizedAnnotations.java
@@ -5,4 +5,5 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface CustomizedAnnotations {
+    String directory();
 }
--- Sprint1/src/main/java/Module7_Annotations/L2exercise1/User.java
+++ Sprint1/src/main/java/Module7_Annotations/L2exercise1/User.java
@@ -1,22 +1,38 @@
 package Module7_Annotations.L2exercise1;
 
-import java.io.FileNotFoundException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-public class User {
-    private String name = "Santiago";
-    private String password = "12345678";
+public class User implements Serializable {
+    private final String name = "Santiago";
+    private final String password = "12345678";
+    static final String CURRENT_PATH = Paths.get("").toAbsolutePath().toString();
 
-    @CustomizedAnnotations
+    public String getName(){
+        return this.name;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    @CustomizedAnnotations(directory = "default_directory")
     public static void customizedSerialization(){
+        Paths.get("").toAbsolutePath().toString();
         User user1 = new User();
-
+        String jsonStr = "";
         try {
-            FileOutputStream fileStream = new FileOutputStream("User.json");
-            ObjectOutputStream out = new ObjectOutputStream(fileStream);
-            out.writeObject(user1);
+            FileOutputStream fileStream = new FileOutputStream(new File("User.json"));
+            ObjectMapper obj = new ObjectMapper();
+            jsonStr = obj.writeValueAsString(user1);
+            fileStream.write(jsonStr.getBytes());
             fileStream.close();
         } catch (IOException err){
             System.out.println(err.getMessage());
