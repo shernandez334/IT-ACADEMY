6680f08d40fb3fc39defcb05136156310aefbfc2
santiago <santiagoso1998@gmail.com>
2024-08-06 11:15:53 +0200

    L1exercise1

---
+++ Sprint1/src/main/java/Module7_Annotations/Worker.java
@@ -0,0 +1,20 @@
+package Module7_Annotations;
+
+public class Worker {
+    private String firstName;
+    private String surname;
+    private double pricePerHour;
+    public Worker(String firstName, String surname, double pricePerHour){
+        this.firstName = firstName;
+        this.surname = surname;
+        this.pricePerHour = pricePerHour;
+    }
+
+    public double getPricePerHour(){
+        return this.pricePerHour;
+    }
+
+    public String calculateSalary(int workingHours){
+        return "";
+    }
+}
