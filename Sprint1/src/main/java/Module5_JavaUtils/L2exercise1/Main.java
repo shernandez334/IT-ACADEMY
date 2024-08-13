4d598cb9a6c38ebfa3a8c8feab7cd844358b0290
santiago <santiagoso1998@gmail.com>
2024-08-13 19:44:03 +0200

    L3exercise1

--- Sprint1/src/main/java/Module5_JavaUtils/L2exercise1/Main.java
+++ Sprint1/src/main/java/Module5_JavaUtils/L2exercise1/Main.java
@@ -5,5 +5,6 @@ import static Module5_JavaUtils.L2exercise1.Directory.sortDirectory;
 public class Main {
     public static void main(String[] arg){
         sortDirectory(System.getProperty("user.dir"), "utils.txt");
+
     }
 }
