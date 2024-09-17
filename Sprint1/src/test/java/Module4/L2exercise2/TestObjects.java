762c179d491365ac8563a906f424d7db1d6a844c
santiago <santiagoso1998@gmail.com>
2024-09-17 20:01:28 +0200

    fixes testObjects

--- Sprint1/src/test/java/Module4/L2exercise2/TestObjects.java
+++ Sprint1/src/test/java/Module4/L2exercise2/TestObjects.java
@@ -8,14 +8,14 @@ public class TestObjects {
     @Test
     public void testEqualObjects(){
         Person person1 = new Person("David", 34);
-        Person person2 = new Person("David", 43);
-        Assertions.assertThat(person1).isSameAs(person1);
+        Person person2 = person1;
+        Assertions.assertThat(person1).isSameAs(person2);
     }
 
     @Test
     public void testNotEqualObjects(){
         Person person1 = new Person("David", 43);
-        Person person2 = new Person("Tony", 90);
+        Person person2 = new Person("David", 43);
         Assertions.assertThat(person1).isNotSameAs(person2);
     }
 }
