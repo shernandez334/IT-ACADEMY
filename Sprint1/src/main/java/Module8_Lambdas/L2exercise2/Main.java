d9c770bd3c3eec620b0400eedcecbfd43db71eed
santiago <santiagoso1998@gmail.com>
2024-09-23 13:39:44 +0200

    fixed lambdas->pairNumbers

--- Sprint1/src/main/java/Module8_Lambdas/L2exercise2/Main.java
+++ Sprint1/src/main/java/Module8_Lambdas/L2exercise2/Main.java
@@ -1,9 +1,6 @@
 package Module8_Lambdas.L2exercise2;
 
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -14,14 +11,7 @@ public class Main {
 
     public static void filterNumbers(List<Integer> numList){
         numList.stream()
-                .filter(n -> (n % 2) != 0)
-                .map(String::valueOf)
-                .map(s -> "o" + s)
-                .forEach(System.out::println);
-        numList.stream()
-                .filter(n -> (n % 2) == 0)
-                .map(String::valueOf)
-                .map(s -> "e" + s)
+                .map(n -> (n % 2 == 0?"e" : "o") + n)
                 .forEach(System.out::println);
     }
 }
